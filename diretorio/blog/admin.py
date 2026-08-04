017a7633248f21e7f7bf78c7b4626a3d28f5f167
Leonardo Lucas monteiro costa <leonardolucas847@gmail.com>
2026-08-04 09:32:43 -0300

    ecrição e edição dos posts dentro do django adminitration por meio do superuser

--- diretorio/blog/admin.py
+++ diretorio/blog/admin.py
@@ -1,5 +1,10 @@
 # Register your models here.
 from django.contrib import admin
 from .models import Post
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug', 'status', 'author', 'created_on')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug':('title',)}
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
