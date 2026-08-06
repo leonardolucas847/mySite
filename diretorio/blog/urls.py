230339a5db7f1ae240c61544e5eca7f70389d6d7
Leonardo Lucas monteiro costa <leonardolucas847@gmail.com>
2026-08-06 10:33:39 -0300

    fix

--- diretorio/blog/urls.py
+++ diretorio/blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from diretorio.blog import views
 from diretorio.blog.views.post_view import PostView
 
-urlspatterns = [
+urlpatterns = [
     path('', views.PostView.as_view(), name='home'),
 ]
